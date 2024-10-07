9397634d91b97d3c2c0c977d63b7f961fb1a7c86
EduardoBarraBalao <ebarrabc2425a@gmail.com>
2024-10-07 18:07:35 +0200

    Comando, directorio de trabajo, variable de entorno USERPROFILE y entorno del proceso

--- src/ProcesoDir.java
+++ src/ProcesoDir.java
@@ -1,16 +1,23 @@
 import java.io.File;
-import java.io.IOException;
+// import java.io.IOException;
 
 public class ProcesoDir {
     public static void main(String[] args) {
-        try {
+        //try {
             ProcessBuilder processBuilder = new ProcessBuilder("dir", "/w /p");
             processBuilder.directory(new File(System.getProperty("user.home")));
-            Process process = processBuilder.start();
-            int exitCode = process.waitFor();
-            System.out.println("Proceso finalizado con código: " + exitCode);
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
+
+            // Comando, directorio de trabajo, variable de entorno USERPROFILE y entorno del proceso
+            System.out.println("Comando: "+processBuilder.command());
+            System.out.println("Directorio de trabajo: "+processBuilder.directory());
+            System.out.println("Variable de entorno USERPROFILE: "+System.getenv("USERPROFILE"));
+            System.out.println("Entorno del proceso: "+processBuilder.environment());
+
+            // Process process = processBuilder.start();
+            // int exitCode = process.waitFor();
+            // System.out.println("Proceso finalizado con código: " + exitCode);
+        // } catch (IOException | InterruptedException e) {
+        //     e.printStackTrace();
+        // }
     }
 } 
